1dd02aff8ee51fae5633cf42f2636362cc2bffaf
Victoria Petrone <65722111+victoria-petrone@users.noreply.github.com>
2025-02-18 13:18:42 +0100

    create Login page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
 
 export default function Header() {
     const activeStyles = {
@@ -13,23 +14,29 @@ export default function Header() {
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
                 <NavLink 
-                    to="/host"
+                    to="host"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
-                    to="/about"
+                    to="about"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
-                    to="/vans"
+                    to="vans"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src={imageUrl}
+                        className="login-icon"
+                    />
+                </Link>
             </nav>
         </header>
     )
