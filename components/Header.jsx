4e3fa3254ff4da17dbf8af399bd7d823efc1e60f
Victoria Petrone <65722111+victoria-petrone@users.noreply.github.com>
2025-02-18 14:33:12 +0100

    multiple changes to finish the app
    create login page with authentication, complete the rest of the pages

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,35 +8,40 @@ export default function Header() {
         textDecoration: "underline",
         color: "#161616"
     }
-    
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink 
-                    to="host"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
-                    to="about"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
-                    to="vans"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
+                    <img
                         src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
