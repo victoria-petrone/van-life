5ad8a479a01041a36dd958dda76a73123474a0be
Victoria Petrone <65722111+victoria-petrone@users.noreply.github.com>
2025-02-18 11:51:27 +0100

    conditional rendering the back button text
    updated back button text to display the applied filter name when a filter is active, defaults to 'Back to all vans' when no filter is applied.

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,8 +1,10 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
+    const location = useLocation()
+    
     const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
@@ -10,9 +12,18 @@ export default function VanDetail() {
             .then(res => res.json())
             .then(data => setVan(data.vans))
     }, [params.id])
-
+    
+    const search = location.state?.search || ""
+    const type = location.state?.type || "all"
+    
     return (
         <div className="van-detail-container">
+            <Link
+                to={`..${search}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to {type} vans</span></Link>
+            
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
